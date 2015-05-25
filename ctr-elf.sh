43fd6710bf181ac927686f95cac7cccb346b9ad9
Yuri Kunde Schlesner <yuriks@yuriks.net>
2015-05-25 19:39:04 -0300

    Improvements to the sh script

--- ctr-elf.sh
+++ ctr-elf.sh
@@ -1,16 +1,20 @@
-#!/bin/bash
+#!/bin/sh -e
 
 WD=$( pwd )
-DIR=$( cd "$( dirname '$0' )" && pwd )
+DIR=$( cd "$( dirname "$0" )" && pwd )
 
 invalid_file() {
 	echo "ERROR: input file is invalid"
 	exit 1
 }
 
-mkdir -p $DIR/workdir
-ctrtool --exefsdir=$DIR/workdir/exefs   "$1" &>/dev/null || invalid_file
-ctrtool --exheader=$DIR/workdir/exh.bin "$1" &>/dev/null || invalid_file
-python $DIR/exefs2elf.py
-mv $DIR/workdir/exefs.elf $WD
-rm -rf $DIR/workdir
+if ! [ -r "$1" ]; then
+	invalid_file
+fi
+
+mkdir -p "$DIR/workdir"
+ctrtool --exefsdir="$DIR/workdir/exefs"   "$1" &>/dev/null || invalid_file
+ctrtool --exheader="$DIR/workdir/exh.bin" "$1" &>/dev/null || invalid_file
+( cd "$DIR" && python exefs2elf.py )
+mv "$DIR/workdir/exefs.elf" "$WD"
+rm -rf "$DIR/workdir"
